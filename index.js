b5d1e6822dee9e6019ab5ff9569ece8cf69318e7
josiah <josiahdamiwilliams@gmail.com>
2020-03-12 11:03:15 +0100

    commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+app.use(express.json())
+app.use(cors())
+const PORT = process.env.PORT || 5000
+app.get('/api', (req, res) => {
+  res.json({ message: 'The api is UP' })
+})
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+})
