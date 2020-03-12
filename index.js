78f151e7b190966f3157b4bc3b577d24cafebc82
josiah <josiahdamiwilliams@gmail.com>
2020-03-12 12:07:32 +0100

    wes

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 require('dotenv').config()
-const express = require('express')
 
+const path =('path')
+//specify a folder fro static assets, like images, css, index.html, js....
+//we need an endpoint on "/" to send index.html
+const express = require('express')
 const cors = require('cors')
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static(path.join(_dirname, 'client/build')))
 
 const PORT = process.env.PORT || 9000
 
 console.log(process.env.LADY);
-
+//endpoints that serve JSON
 app.get('/api', (req, res) => {
   res.json({ message: 'The api is UP' })
+})
+
+//fallback endpoint that will just send back index.html with the CRA
+app.get("*", (req, res) => {
+    //send back the index.html contained inside client/build
+    res.sendFile(path.join(_dirname, 'client/build', 'index.html'))
 })
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
